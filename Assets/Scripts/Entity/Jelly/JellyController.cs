eebe70b4f1ab7808a348a0938cf96b66147c0585
lolaop5047 <kthh9828@naver.com>
2025-02-24 11:08:50 +0900

    ✨ Feat: “ 젤리 오브젝트 충돌 삭제”
    Player가 젤리를 충돌하면 삭제 와 함께 점수 추가.

--- Assets/Scripts/Entity/Jelly/JellyController.cs
+++ Assets/Scripts/Entity/Jelly/JellyController.cs
@@ -6,9 +6,11 @@ public class JellyController : MonoBehaviour
 {
     public int scoreValue = 10; // 젤리를 먹으면 얻는 점수
 
+    //public int scoreValue = 10; // 젤리 점수
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) // 플레이어가 충돌했는지 확인
+        if (other.CompareTag("Player")) // 충돌한 객체가 Player인지 확인
         {
             //GameManager.Instance.AddScore(scoreValue); // 점수 추가
             Destroy(gameObject); // 젤리 삭제
