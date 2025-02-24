bc68c25f608b218d28e502f59c8b405e4e05e639
hannah896 <phn2260@naver.com>
2025-02-24 16:47:03 +0900

    🐛 Fix: “젤리메이커 로직 수정”
    젤리메이커 끝!!!

--- Assets/Scripts/Entity/Jelly/JellyMaker.cs
+++ Assets/Scripts/Entity/Jelly/JellyMaker.cs
@@ -6,32 +6,33 @@ using UnityEngine.Rendering;
 
 public class JellyMaker : MonoBehaviour
 {
-    Vector2 pivot;
-    Vector2 position;
-
-    //길이1번 길이 2번
-    Vector2 posA;
-    Vector2 posB;
     Queue<Collider2D> obstacleQueue = new Queue<Collider2D>();
 
-    float length = 1;
+    //젤리 부모오브젝트
+    public GameObject JellyObject; 
+    //생성되는 젤리 프리펩
+    public GameObject jellyPrefab;
+    //이전에 설치된 젤리 오브젝트
+    private GameObject preJelly;
 
-    public GameObject jelly;
-    public GameObject JellyObject;
+    //젤리가 도달해야하는 y값
+    Vector2 pivot;
+
+    //그라운드의 기본 pivot값
+    Vector2 groundVector = new Vector2(20f, -3f);
+
+    //길이를 구하는 벡터 posA가 메이커의 벡터
+    Vector3 posA;
+
+    float length = 1f;
+    float time = 0f;
+
 
     private void Awake()
     {
-        pivot = new Vector2(20f, -3f);
-        position = transform.position;
+        pivot = groundVector;
         posA = transform.position;
-    }
-
-    //컴퓨터 성능에 따라 생성
-    private void Update()
-    {
-        position.x += Time.deltaTime;
-
-        transform.position = position;
+        MakeJelly(posA);
     }
 
     ////1초에 50번 생성
@@ -39,48 +40,58 @@ public class JellyMaker : MonoBehaviour
     //{
     //    //일정 시간간격으로 젤리를 생성함
     //    time += Time.fixedDeltaTime;
-
-    //    if (time >= 1)
+    //    posB = transform.position;
+    //    if (time >= 0.2f)
     //    {
     //        time = 0;
     //        MakeJelly(position);
+    //    }
+
+    //    if ((posB - posA).magnitude >= length)
+    //    {
+    //        MakeJelly(posB);
+    //        posA = posB;
     //    }
 
     //    //피벗 값 받아서 러프 적용?
     //    position.y = Mathf.Lerp(position.y, pivot.y, 0.25f);
     //}
 
-    //1초에 50번 생성
-    private void FixedUpdate()
+    private void Update()
     {
-        posB = transform.position;
-
-        
-        if ((posB - posA).magnitude >= length)
+        //일정 간격으로 젤리를 생성하기
+        if ((preJelly.transform.position - posA).magnitude >= length)
         {
-            MakeJelly(posB);
-            posA =posB;
+            MakeJelly(posA);
         }
-        
-        position.y = Mathf.Lerp(position.y, pivot.y, 0.25f);
+
+        //젤리의 y값 위치 수정
+        posA.y = Mathf.Lerp(posA.y, pivot.y, 0.1f);
     }
 
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        pivot = collision.transform.Find("Pivot").transform.position;
-        obstacleQueue.Enqueue(collision);
+        if (collision.CompareTag("Obstacle"))
+        {
+            Debug.Log(collision.name);
+            pivot = collision.transform.Find("Pivot").transform.position;
+            obstacleQueue.Enqueue(collision);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        obstacleQueue.Dequeue();
-        if (obstacleQueue.Count == 0) pivot.y = -3f;
+        if (collision.CompareTag("Obstacle"))
+        {
+            obstacleQueue.Dequeue();
+            if (obstacleQueue.Count == 0) pivot.y = -3f;
+        }
     }
 
     private void MakeJelly(Vector2 pos)
     {
-        Instantiate(jelly, pos, Quaternion.identity, JellyObject.transform);
+        preJelly = Instantiate(jellyPrefab, pos, Quaternion.identity, JellyObject.transform);
     }
 }
