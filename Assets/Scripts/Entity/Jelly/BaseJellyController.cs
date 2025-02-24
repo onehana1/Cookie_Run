26e0dd56fcec6c00fa684c0f62d0614199e4bf4f
hannah896 <phn2260@naver.com>
2025-02-24 17:59:11 +0900

    🙈 WIP: “젤리 작업중”
    젤리 개별스크립트 -> BaseJellyController로 수정했습니다

--- Assets/Scripts/Entity/Jelly/BaseJellyController.cs
+++ Assets/Scripts/Entity/Jelly/BaseJellyController.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rain_JellyController : MonoBehaviour
+public class Base_JellyController : MonoBehaviour
 {
-    public int scoreValue = 25; // 젤리를 먹으면 얻는 점수
-
-    //public int scoreValue = 25; // 젤리 점수
+    public int scoreValue; // 젤리를 먹으면 얻는 점수
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,16 +13,5 @@ public class Rain_JellyController : MonoBehaviour
             //GameManager.Instance.AddScore(scoreValue); // 점수 추가
             Destroy(gameObject); // 젤리 삭제
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
+
+    //Á¡¼ö
+    public int Score;
 
     public static GameManager Instance
     {
@@ -18,15 +22,8 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    internal void AddScore(int scoreValue)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Score += scoreValue;
     }
 }
