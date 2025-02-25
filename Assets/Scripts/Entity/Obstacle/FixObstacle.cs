0493c80610d64a2139069039c1afe52ce3c23ef7
royun0307 <109058557+royun0307@users.noreply.github.com>
2025-02-25 15:50:54 +0900

    ✨ Feat: “장애물 애니메이션 구현”
    장애물 애니메이션 구현

--- Assets/Scripts/Entity/Obstacle/FixObstacle.cs
+++ Assets/Scripts/Entity/Obstacle/FixObstacle.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class FixObstacle : BaseObstacle
 {
+    public Animator animator;
+    public bool isMove;
+    private void Awake()
+    {
+        isMove = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (animator != null && !isMove && transform.position.x < 0)
+        {
+            isMove = true;
+            animator.SetTrigger("Move");
+        } 
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.gameObject.TryGetComponent<BaseState>(out baseState);
