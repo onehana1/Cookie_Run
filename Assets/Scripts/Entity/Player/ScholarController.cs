9df02bc91f7469392736ba587f1364581f0f5069
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-02-25 14:03:16 +0900

    ✨ Feat: “유생 스킬 자동으로 나감”
    - auto 끄면 자동으로 나가지 않습니다.

--- Assets/Scripts/Entity/Player/ScholarController.cs
+++ Assets/Scripts/Entity/Player/ScholarController.cs
@@ -4,31 +4,68 @@ using UnityEngine;
 
 public class ScholarController : BaseController
 {
-    [SerializeField] private float skillDurationTime;
-    [SerializeField] private float skillCollTime;
+    [Header("Scholar")]
+    [SerializeField] private float skillCollTime; // 스킬 쿨타임
+    [SerializeField] private float skillDurationTime = 5;
+
     [SerializeField] private GameObject skillEffect;
     [SerializeField] private Animator skillAnimator;
+    [SerializeField] private bool isAutoSkillActive = true;
 
+    private float skillCooldown = 5;
 
-
+    private void Awake()
+    {
+        skillDuration = skillDurationTime;
+    }
     protected override void Start()
     {
         base.Start();
         originalColliderSize = new Vector2(0.6f, 0.82f);
         slideColliderSize = new Vector2(originalColliderSize.x, 0.5f);
+        skillCooldown = skillCollTime;
+
+        StartCoroutine(DelayedAutoSkillStart());
     }
 
     protected override void Update()
     {
         base.Update();
+        if (skillCooldown > 0)
+        {
+            skillCooldown -= Time.deltaTime;
+        }
+
         HandleSkill();
     }
-
+    private IEnumerator DelayedAutoSkillStart()
+    {
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(AutoSkill());
+    }
     private void HandleSkill()
     {
         if (Input.GetKeyDown(KeyCode.F))  // 예: 스킬 키가 F
         {
+            if (skillCooldown > 0) return;
+
             StartCoroutine(Skill());
+        }
+
+
+    }
+    private IEnumerator AutoSkill()
+    {
+        Debug.Log("AutoSkill 시작");
+        while (isAutoSkillActive)
+        {
+            if (skillCooldown <= 0)
+            {
+                Debug.Log("AutoSkill");
+                yield return StartCoroutine(Skill());
+            }
+
+            yield return new WaitForSeconds(skillCooldown > 0 ? skillCooldown : 0.1f);
         }
     }
 
@@ -41,14 +78,16 @@ public class ScholarController : BaseController
 
         float animationLength = skillAnimator.GetCurrentAnimatorStateInfo(0).length;
         skillAnimator.speed = animationLength / skillDuration;
+        skillCooldown = skillCollTime;
 
-        yield return new WaitForSeconds(skillDuration);
+        yield return new WaitForSeconds(skillDuration); // 스킬 지속 시간
 
         animationHandler.SetSkill(false);
         baseState.isInvincible = false;
         skillEffect.SetActive(false);
 
         skillAnimator.speed = 1.0f;
+
         baseState.StartInvincibility(invinvibleTime);
         StartBlinkEffect(invinvibleTime);
 
