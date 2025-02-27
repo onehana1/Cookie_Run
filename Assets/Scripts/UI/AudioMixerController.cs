29819f3cdfae4dc5637bd1e0166f6e7e3d517dbe
royun0307 <109058557+royun0307@users.noreply.github.com>
2025-02-27 19:36:15 +0900

    ✨ Feat: “사운드 설정 저장”

--- Assets/Scripts/UI/AudioMixerController.cs
+++ Assets/Scripts/UI/AudioMixerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -21,29 +22,38 @@ public class AudioMixerController : MonoBehaviour
         if (MasterSlider != null)
         {
             MasterSlider.onValueChanged.AddListener(SetMasterValue);
+            MasterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
+            SetMasterValue(MasterSlider.value);
         }
         if (BGMSlider != null)
         {
             BGMSlider.onValueChanged.AddListener(SetBGMValue);
+            BGMSlider.value = PlayerPrefs.GetFloat("BGMVolume", 1f);
+            SetBGMValue(BGMSlider.value);
         }
         if (EffectSlider != null)
         {
             EffectSlider.onValueChanged.AddListener(SetEffectValue);
+            EffectSlider.value = PlayerPrefs.GetFloat("EffectVolume", 1f);
+            SetEffectValue(EffectSlider.value);
         }
     }
 
     public void SetMasterValue(float volume)
     {
+        PlayerPrefs.SetFloat("MasterVolume", volume);
         audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
     }
 
     public void SetBGMValue(float volume)
     {
+        PlayerPrefs.SetFloat("BGNVolume", volume);
         audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
     }
 
     public void SetEffectValue(float volume)
     {
+        PlayerPrefs.SetFloat("EffectVolume", volume);
         audioMixer.SetFloat("Effect", Mathf.Log10(volume) * 20);
     }
 }
