2c87eb6e35b8d3860ff47a7c497c60c68ce16be6
hannah896 <phn2260@naver.com>
2025-02-27 11:33:27 +0900

    ✨ Feat: “스테이지 씬 연결”
    인피니트 모드, 스토리 모드 둘다 연결 시켜놨습니다.
    플레이매니저로 모드 둘다 연결시킬예정 +_+
    
    GameManager에 이전 게임의 스코어 기록들, 이전판의 획득코인 필드 추가 했습니다.

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public  class ChangeScene : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {   
     public static void ChangeLobbyScene()
     {
